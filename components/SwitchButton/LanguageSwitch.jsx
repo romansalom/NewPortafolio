c0ce9fda76051fc251d5f0db4c892e5f7ae2343e
rolo salom <roman.salom12@gmail.com>
2025-02-26 12:44:36 -0300

    background shadow works

--- components/SwitchButton/LanguageSwitch.jsx
+++ components/SwitchButton/LanguageSwitch.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import { useLanguage } from "../../context/LanguageContext.jsx";
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
 export default function LanguageSwitch() {
     const { language, toggleLanguage } = useLanguage();
+    const [blur, setBlur] = useState(false);
+
+    const handleToggle = () => {
+        setBlur(true); // Activa el blur
+        setTimeout(() => {
+            toggleLanguage(); // Cambia el idioma
+            setBlur(false); // Desactiva el blur
+        }, 500); // Duración del efecto
+    };
 
     return (
-        <div className={styles.switch_btn} onClick={toggleLanguage}>
-            <span className={styles.slider}>{language === "En" ? "En" : "Es"}</span>
+        <>
+            {/* Fondo borroso cuando cambia el idioma */}
+            {blur && <div className={styles.overlay}></div>}
 
-        </div>
+            {/* Botón de cambio de idioma */}
+            <div className={styles.switch_btn} onClick={handleToggle}>
+                <span className={styles.slider}>
+                    {language === "En" ? "En" : "Es"}
+                </span>
+            </div>
+        </>
     );
 }
