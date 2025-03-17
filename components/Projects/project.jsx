2eeef9699a9bd0f04ae5ee6af04cbb37aa0107b0
rolo salom <roman.salom12@gmail.com>
2025-03-17 12:12:24 -0300

    card structure

--- components/Projects/project.jsx
+++ components/Projects/project.jsx
@@ -8,15 +8,21 @@ export default function Proyects() {
 
 
     return (
-        <div >
+        <div className={styles.container} >
 
-            <div className={styles.container}>
-                <div>
-                    <h2>Card Title</h2>
-                    <h3>Tecnologies</h3>
-                    <div><ul><li>tec1</li><li>tec2</li><li>tec3</li><li>tec4</li></ul></div>
-                </div>
+            <div className={styles.cardContainer}>
+                <h2 >Card Titles</h2>
+                <img src="https://i.blogs.es/7289a5/captura-de-pantalla-2016-01-07-a-las-17.29.49/1366_2000.png"></img>
+                <h3>Tecnologies</h3>
+                <div><ul><li>tec1</li><li>tec2</li><li>tec3</li><li>tec4</li></ul></div>
             </div>
+            <div className={styles.cardContainer}>
+                <h2 >Card Titles</h2>
+                <img src="https://i.blogs.es/7289a5/captura-de-pantalla-2016-01-07-a-las-17.29.49/1366_2000.png"></img>
+                <h3>Tecnologies</h3>
+                <div><ul><li>tec1</li><li>tec2</li><li>tec3</li><li>tec4</li></ul></div>
+            </div>
+
         </div>
 
 
