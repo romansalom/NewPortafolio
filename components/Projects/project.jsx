9024e4f4021c9882b60145e925f76e1917be2ff7
rolo salom <roman.salom12@gmail.com>
2025-03-17 11:57:17 -0300

    navbar style

--- components/Projects/project.jsx
+++ components/Projects/project.jsx
@@ -1,6 +1,7 @@
 // components/ItalicsName.js
 "use client"
 
+import styles from "./styles.module.css";
 
 
 export default function Proyects() {
@@ -9,7 +10,13 @@ export default function Proyects() {
     return (
         <div >
 
-            Proyects
+            <div className={styles.container}>
+                <div>
+                    <h2>Card Title</h2>
+                    <h3>Tecnologies</h3>
+                    <div><ul><li>tec1</li><li>tec2</li><li>tec3</li><li>tec4</li></ul></div>
+                </div>
+            </div>
         </div>
 
 
