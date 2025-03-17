5db9c3c9d5b336762c6b76431b038032c1631a57
rolo salom <roman.salom12@gmail.com>
2025-03-17 12:35:34 -0300

    card and nav bar styles

--- components/Projects/project.jsx
+++ components/Projects/project.jsx
@@ -11,17 +11,27 @@ export default function Proyects() {
         <div className={styles.container} >
 
             <div className={styles.cardContainer}>
-                <h2 >Card Titles</h2>
-                <img src="https://i.blogs.es/7289a5/captura-de-pantalla-2016-01-07-a-las-17.29.49/1366_2000.png"></img>
-                <h3>Tecnologies</h3>
-                <div><ul><li>tec1</li><li>tec2</li><li>tec3</li><li>tec4</li></ul></div>
+                <div className={styles.cardContent} >
+
+                    <h2 >Card Titles</h2>
+                    <img src="https://i.blogs.es/7289a5/captura-de-pantalla-2016-01-07-a-las-17.29.49/1366_2000.png"></img>
+                    <h3>Tecnologies</h3>
+                    <div><ul><li>tec1</li><li>tec2</li><li>tec3</li><li>tec4</li></ul></div>
+                </div>
+
             </div>
+
             <div className={styles.cardContainer}>
-                <h2 >Card Titles</h2>
-                <img src="https://i.blogs.es/7289a5/captura-de-pantalla-2016-01-07-a-las-17.29.49/1366_2000.png"></img>
-                <h3>Tecnologies</h3>
-                <div><ul><li>tec1</li><li>tec2</li><li>tec3</li><li>tec4</li></ul></div>
+                <div className={styles.cardContent} >
+
+                    <h2 >Card Titles</h2>
+                    <img src="https://i.blogs.es/7289a5/captura-de-pantalla-2016-01-07-a-las-17.29.49/1366_2000.png"></img>
+                    <h3>Tecnologies</h3>
+                    <div><ul><li>tec1</li><li>tec2</li><li>tecs3</li><li>tec4</li></ul></div>
+                </div>
+
             </div>
+
 
         </div>
 
